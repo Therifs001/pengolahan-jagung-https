fb861d43f1d994da15710d1c7876a43cdffad3bc
Defina Apriliani <defina208@gmail.com>
2024-11-29 09:14:00 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const Inert = require('@hapi/inert');
 const init = async () => {
   const server = Hapi.server({
     port: 3000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
   });
 
   await server.register(Inert);
