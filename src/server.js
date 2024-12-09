ed9a85dcb0d5e4f433ae2caaf5cb46ca6568a4a3
Therifs001 <135795408+Therifs001@users.noreply.github.com>
2024-12-09 09:14:23 +0700

    Update server.js
    up

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
-    port: process.env.PORT || 3000,
+    port: process.env.PORT,
     host: '0.0.0.0',
   });
 
