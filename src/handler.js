efa58837394206b65d36fc31d7891653dc1ff031
Therifs001 <135795408+Therifs001@users.noreply.github.com>
2024-12-10 03:08:56 +0700

    handler.js

--- src/handler.js
+++ src/handler.js
@@ -1,10 +1,30 @@
-const recoms = require('./recoms'); 
+const recoms = require('./recoms');
 
 module.exports = {
-    getHome: () => {
-        return { message: 'Welcome to Pengolahan Jagung!' };
+    // Handler untuk route home
+    getHome: (request, h) => {
+        return h.response({ message: 'Welcome to Pengolahan Jagung!' }).code(200);
     },
-    getRecommendations: () => {
-        return recoms; 
-    }
+
+    // Handler untuk route mendapatkan semua rekomendasi
+    getRecommendations: (request, h) => {
+        return h.response(recoms).code(200); 
+    },
+
+    // Handler untuk mendapatkan rekomendasi berdasarkan ID
+    getRecommendationById: (request, h) => {
+        const { id } = request.params;
+        const numericId = Number(id); // Konversi ID ke angka jika diperlukan
+
+        const recommendation = recoms.find(recom => recom.id === numericId);
+
+        if (!recommendation) {
+            return h.response({
+                error: true,
+                message: 'selamat datang',
+            }).code(404);
+        }
+
+        return h.response(recommendation).code(200);
+    },
 };
